887cbec564362f01d719d568e33c1e46ded6356f
Luis Cadillo <luiscaf3r@gmail.com>
2024-08-07 18:22:19 -0500

    test
    testing gitbutler

---
+++ floating-view/src/main/java/com/floatingview/library/Test.kt
@@ -0,0 +1,10 @@
+package com.floatingview.library
+
+import android.util.Log
+
+fun Test() {
+  Log.d("dfkdk", "kdfkd")
+  Log.d("dfkdk", "kdfkd")
+  Log.d("dfkdk", "kdfkd")
+  Log.d("MAIN virtual", "")
+}
